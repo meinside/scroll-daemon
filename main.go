ee77f41464a07bf6a0b39459c7a65c3336065a73
Sungjin Han <meinside@gmail.com>
2016-10-04 14:24:54 +0900

    Remove vendor directory

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/meinside/scroll-daemon/lib"
 
 	scroll "github.com/meinside/scrollphat-go"
-	bot "github.com/meinside/telegram-bot-go" // version 0.1.1
+	bot "github.com/meinside/telegram-bot-go"
 )
 
 const (
